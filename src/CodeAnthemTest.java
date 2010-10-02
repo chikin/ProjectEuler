6e01b4d699142664e9cc750208417c96be316265
Trevor Whitlock <twhitlock@gmail.com>
2010-10-02 15:57:38 -0600

    Initializing the repository

---
+++ src/CodeAnthemTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: twhitlock
+ * Date: Sep 16, 2010
+ * Time: 9:46:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CodeAnthemTest {
+}
