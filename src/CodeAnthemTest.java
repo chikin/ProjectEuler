4d95a7f54033422d03582ecd94c1c412166b5acd
twhitlock <twhitlock@gmail.com>
2014-02-26 14:17:51 -0800

    Finished Problem 1.

--- src/CodeAnthemTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: twhitlock
- * Date: Sep 16, 2010
- * Time: 9:46:33 PM
- * To change this template use File | Settings | File Templates.
- */
-public class CodeAnthemTest {
-}
---
+++ src/com/twhitlock/Problem1.groovy
@@ -0,0 +1,29 @@
+package com.twhitlock
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+ */
+class Problem1 {
+
+  def sum = 0
+  def limit = 1000
+  def index = 0
+  def calculate() {
+      while(index < limit) {
+        if(index % 3 == 0 || index % 5 == 0) {
+            sum = sum + index
+        }
+        index = index + 1
+      }
+    println sum
+  }
+
+  public static void main(String[] args) {
+      def problem1 = new Problem1()
+      problem1.calculate()
+  }
+
+
+}
