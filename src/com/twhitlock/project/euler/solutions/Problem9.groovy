567ad97c8f230921bbbdf007f0e4a881d4f5303b
twhitlock <twhitlock@gmail.com>
2014-03-04 21:18:02 -0800

    Finishing problem 9

---
+++ src/com/twhitlock/project/euler/solutions/Problem9.groovy
@@ -0,0 +1,25 @@
+package com.twhitlock.project.euler.solutions
+
+/**
+ * Created by twhitlock on 3/4/14.
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+ a2 + b2 = c2
+ For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ Find the product abc.
+ */
+
+for(int a = 0; a < 1000 ; a++) {
+    def aSquared = a * a;
+    for(int b = 0; b < 1000; b++) {
+        def bSquared = b * b;
+        def c = Math.sqrt(aSquared +bSquared )
+        if(a + b + c == 1000) {
+            println "a: ${a} b: ${b} c: ${c}"
+            break
+        }
+    }
+}
+
